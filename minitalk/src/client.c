1f0e5e99520b0632571f0861e68310fd4f9ac414
wsl <elkhailiissam0@gmail.com>
2025-03-29 00:51:45 +0100

    hel

---
+++ minitalk/src/client.c
@@ -0,0 +1,65 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   client.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: isel-kha <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/21 00:00:00 by isel-kha          #+#    #+#             */
+/*   Updated: 2025/02/28 22:59:51 by isel-kha         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minitalk.h"
+
+void	ft_send_char(int pid, unsigned char c)
+{
+	int	bit;
+
+	bit = 0;
+	while (bit < 8)
+	{
+		if ((c & (1 << bit)) != 0)
+			kill(pid, SIGUSR2);
+		else
+			kill(pid, SIGUSR1);
+		usleep(900);
+		bit++;
+	}
+}
+
+void	ft_send_string(int pid, char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		ft_send_char(pid, str[i]);
+		i++;
+	}
+	ft_send_char(pid, '\0');
+}
+
+int	main(int argc, char **argv)
+{
+	int	pid;
+
+	if (argc != 3)
+	{
+		ft_putstr_fd("Usage: ./client [server_pid] [string_to_send]\n", 1);
+		return (1);
+	}
+	if ((ft_atoi(argv[1])) == 2147483647)
+	{
+		ft_putstr_fd("Invalid Pid\n", 1);
+	}
+	pid = ft_atoi(argv[1]);
+	if (pid <= 0)
+	{
+		ft_putstr_fd("Error: Invalid PID\n", 1);
+		return (1);
+	}
+	ft_send_string(pid, argv[2]);
+	return (0);
+}
---
+++ minitalk/src/server.c
@@ -0,0 +1,73 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   server.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: isel-kha <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/21 00:00:00 by isel-kha          #+#    #+#             */
+/*   Updated: 2025/02/28 22:54:15 by isel-kha         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minitalk.h"
+
+t_data	g_data;
+
+void	ft_handle_signal(int sig, siginfo_t *info, void *context)
+{
+	(void)context;
+	g_data.client_pid = info->si_pid;
+	if (sig == SIGUSR2)
+		g_data.c |= (1 << g_data.bit_position);
+	g_data.bit_position++;
+	if (g_data.bit_position == 8)
+	{
+		if (g_data.c == 0)
+			ft_putchar_fd('\n', 1);
+		else
+			ft_putchar_fd(g_data.c, 1);
+		g_data.c = 0;
+		g_data.bit_position = 0;
+	}
+}
+
+void	ft_setup_signals(void)
+{
+	struct sigaction	sa;
+
+	ft_memset(&sa, 0, sizeof(sa));
+	sa.sa_sigaction = ft_handle_signal;
+	sa.sa_flags = SA_SIGINFO;
+	sigemptyset(&sa.sa_mask);
+	sigaction(SIGUSR1, &sa, NULL);
+	sigaction(SIGUSR2, &sa, NULL);
+}
+
+void	ft_print_pid(void)
+{
+	pid_t	pid;
+
+	pid = getpid();
+	ft_putstr_fd("Server PID: ", 1);
+	ft_putnbr_fd(pid, 1);
+	ft_putstr_fd("\n", 1);
+}
+
+int	main(int argc, char **argv)
+{
+	if (argc != 1)
+	{
+		ft_putstr_fd("Usage: ./server\n", 1);
+		return (1);
+	}
+	ft_atoi(&argv[0][1]);
+	g_data.c = 0;
+	g_data.bit_position = 0;
+	g_data.client_pid = 0;
+	ft_print_pid();
+	ft_setup_signals();
+	while (1)
+		pause();
+	return (0);
+}
